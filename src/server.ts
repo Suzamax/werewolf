170693c1b382c751d2f15c115365fde829debe78
Carlos <ratiosu.cct@gmail.com>
2020-04-02 19:03:13 +0200

    Socket works!

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import "reflect-metadata"; // this shim is required
 import {createExpressServer} from "routing-controllers";
-
 import { VillageController } from "./controllers/VillageController";
 
 import * as express from 'express';
 import * as path from 'path';
 import * as http from 'http';
-import * as WebSocket from 'ws';
 import * as mustacheExpress from 'mustache-express';
+import * as socketio from 'socket.io';
+import {Socket} from "socket.io";
 
 const PORT = process.env.PORT || 3000;
 const app = createExpressServer({
@@ -21,24 +21,23 @@ app
     .use(express.static(path.join(__dirname,'/public')))
     .set('views', __dirname + '/views')
     .set('view engine', 'mustache')
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 // initialize a simple http server
-const server = http.createServer(app);
+const httpsrv = http.createServer(app);
+const io = socketio(httpsrv);
 
-// initialize the WebSocket server instance
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', (ws: WebSocket) => {
-
+io.on('connection', (socket: Socket) => {
+    console.log("An user connected!")
     // connection is up, let's add a simple simple event
-    ws.on('message', (message: string) => {
+    socket.on('message', (message: string) => {
 
         // log the received message and send it back to the client
         console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
+        socket.send(`Hello, you sent -> ${message}`);
     });
 
     // send immediatly a feedback to the incoming connection
-    ws.send('Hi there, I am a WebSocket server');
+    socket.send('Hi there, I am a WebSocket server');
 });
+
+const server = httpsrv.listen(PORT, () => console.log(`Listening on ${PORT}`));
